fd61b776134188bd927c1b9e3137403da2beb1ee
Matthew Perry <perrygeo@gmail.com>
2014-12-17 13:01:37 -0800

    script to publish ghpages

---
+++ publish.sh
@@ -0,0 +1,10 @@
+cp -r index.html css/ data/ js/ output
+MSG="publish gh-pages"
+# requires pip install ghp-import
+ghp-import output -m "$MSG"
+git push origin gh-pages
+echo
+echo "DON'T FORGET TO COMMIT/PUSH MASTER"
+echo "git status"
+echo "git commit -a -m \"$MSG\""
+echo "git push origin master" 
