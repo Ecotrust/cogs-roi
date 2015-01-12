21fd4a5749313a152219c017d008578ce53e7469
Matthew Perry <perrygeo@gmail.com>
2015-01-12 15:27:38 -0800

    document publish workflow

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ cogs-roi
 ========
 
 COGS Return on Investment Explorer
+
+
+# Build instructions
+
+Work in master gets published to the `gh-pages` branch of the same repo. To make
+this process easier, you can run the `publish.sh` script from the root dir.
+
+This will compile the static files into an output directory,
+commit that output directory as the new gh-pages branch and
+push changes to github. Any tweaks to the routine should be handled by the 
+`publish.sh` script
--- publish.sh
+++ publish.sh
@@ -1,14 +1,17 @@
 rm -rf output
+
+# copy static content
 mkdir output
 cp -r docs.html index.html css/ js/ output
+
+# only copy the topojson data
 mkdir output/data
 cp -r data/ecoregions.topojson output/data
+
 MSG="publish gh-pages"
 # requires pip install ghp-import
 ghp-import output -m "$MSG"
 git push origin gh-pages
+
 echo
-echo "DON'T FORGET TO COMMIT/PUSH MASTER"
-echo "git status"
-echo "git commit -a -m \"$MSG\""
-echo "git push origin master" 
+echo "gh-pages branch published; don't forget to push to master as well"
